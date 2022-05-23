5cb3df06001f72599b1061edeb34f39f5e75bc5e
Eric Sundqvist <es225kz@student.lnu.se>
2022-05-23 11:54:03 +0200

    Edit Start page paragraphs

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -7,23 +7,21 @@ function Start() {
   return (
     <Container component="main" maxWidth="md">
       <h1>Landing Page</h1>
-      <p>Welcome! Bla bla bla</p>
+      <p>Welcome!</p>
+      <p>
+        Climbing Partners is an applikation that helps you find partners to climb with whereever you
+        go! Create an account and start using the application now.
+      </p>
       <Box marginBottom={2}>
-        <Button variant="contained" component={RouterLink} to="/login">
-          Log in
-        </Button>
-      </Box>
-      <Box>
         <Button variant="contained" component={RouterLink} to="/signup">
           Sign up
         </Button>
       </Box>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci tempora labore, iste quis
-        maxime aspernatur atque. Beatae optio incidunt facere quasi nam quae vitae, necessitatibus
-        ipsum eos alias illum dolor? Suscipit iste corrupti impedit quisquam! Molestiae animi eius
-        dolore suscipit.
-      </p>
+      <Box>
+        <Button variant="contained" component={RouterLink} to="/login">
+          Log in
+        </Button>
+      </Box>
     </Container>
   );
 }
