319c14d1e1138c32a10487c451a0f00d6666f5cf
Eric Sundqvist <es225kz@student.lnu.se>
2022-05-23 14:49:13 +0200

    Update No match component content

--- src/pages/NoMatch.jsx
+++ src/pages/NoMatch.jsx
@@ -1,13 +1,31 @@
-import { Link } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 function NoMatch() {
   return (
-    <div>
-      <h2> 404 - Nothing to see here!</h2>
-      <p>
-        <Link to="/">Go to the home page</Link>
-      </p>
-    </div>
+    <Container
+      maxWidth="md"
+      sx={{
+        height: '70vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 2,
+      }}
+    >
+      <Typography variant="h3" color="initial">
+        404
+      </Typography>
+      <Typography variant="h5" color="initial">
+        Nothing to see here!
+      </Typography>
+      <Button component={RouterLink} to="/" variant="text" color="primary">
+        Go to home page
+      </Button>
+    </Container>
   );
 }
 
